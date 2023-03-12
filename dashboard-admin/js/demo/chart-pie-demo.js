15c94513f301568a88e7181989e5c37f9943a206
Silva <ritatsilva@deloitte.pt>
2023-03-12 05:03:36 +0000

    Dashboard- Atualização de CSS e mais traduções

--- dashboard-admin/js/demo/chart-pie-demo.js
+++ dashboard-admin/js/demo/chart-pie-demo.js
@@ -7,11 +7,10 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Direta", "Social", "Referência"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#f4b30d', '#888580', '#c81c1c'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
